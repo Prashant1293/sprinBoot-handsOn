3aa5c06c7413fe3b4e1147b361d946f059a3ab5c
Prashant Sharma <PrashantSharma@rccl.com>
2020-02-10 23:01:08 +0530

    Added working implementation of a web client to capture a 3rd party response.

---
+++ src/main/java/edu/knoldus/com/handsonspringboot/controller/MockWebClientController.java
@@ -0,0 +1,41 @@
+package edu.knoldus.com.handsonspringboot.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RequestMapping("/web/clients")
+@RestController
+@Slf4j
+public class MockWebClientController {
+    
+    @Value("${base.url:NOT-FOUND}")
+    private String baseUrl;
+    
+    public MockWebClientController() {
+        log.info("Web Client initialized with base url = {} ... ", this.baseUrl);
+    }
+    
+    @GetMapping(path = "/{clientId}")
+    @ResponseBody
+    public Mono<String> getUserFromWebClient(@PathVariable String clientId) {
+        
+        final Mono<String> result = WebClient.builder()
+                .baseUrl(baseUrl)
+                .build()
+                .get()
+                .uri("/v2/{clientId}", clientId)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String.class);
+        
+        return result;
+    }
+}
